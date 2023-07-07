65d3af673a5a062118e5736abea2c7b8bf072890
Mayur Kumar <kmayur809@gmail.com>
2023-07-07 13:23:27 +0530

    graphql-request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import {} from "graphql";
+import { GraphQLClient } from "graphql-request";
 
 export default function Home() {
   return (
